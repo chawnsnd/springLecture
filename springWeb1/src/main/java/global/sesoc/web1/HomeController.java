2a2619d6e193b0ab044ad4e187196d18b713b82d
Junwoong Cha <chawnsnd@gmail.com>
2020-01-14 15:17:39 +0900

    web1 login, join에 관한 controller, view

---
+++ springWeb1/src/main/java/global/sesoc/web1/HomeController.java
@@ -0,0 +1,55 @@
+package global.sesoc.web1;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+		
+	// 메인화면으로 이동
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		return "home";
+	}
+
+	// 회원가입화면으로 이동
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
+	public String goJoinForm(Locale locale, Model model) {
+		return "joinForm";
+	}
+
+	// 회원가입화면으로 이동
+	@RequestMapping(value = "/join2", method = RequestMethod.POST)
+	public String join(String id, String password, String name, String phone) {
+		System.out.println("======== 회원가입폼에서 입력받은 값 ============");
+		System.out.println("ID : "+id);
+		System.out.println("비밀번호 : "+password);
+		System.out.println("이름 : "+name);
+		System.out.println("전화번호 : "+phone);
+		
+		return "redirect:/";
+		/*
+		 * 리다이렉트 : 클라이언트가 다른 URL로 움직이게 하기 return "redirect:/login"
+		 * 포워드 : 토스 return "login";
+		 */
+	}
+	
+	// 로그인화면으로 이동
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String goLoginForm(Locale locale, Model model) {
+		return "loginForm";
+	}
+
+	// 로그인화면으로 이동
+	@RequestMapping(value = "/login2", method = RequestMethod.POST)
+	public String login(String id, String password) {
+		System.out.println("======== 로그인폼에서 입력받은 값 ============");
+		System.out.println("ID : "+id);
+		System.out.println("비밀번호 : "+password);
+		return "redirect:/";
+	}
+}
---
+++ springWeb1/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home</title>
+</head>
+<body>
+	<h1>안뇽하살법!</h1>
+	<P>받아치기</P>
+
+	<!-- 절대경로는 절대로 쓰지 말것 -->
+	<!-- <a href="https://123.123.123.123:8888/web1/join">회원가입하러가기</a> -->
+	<!-- <a href="/web1/join">회원가입하러가기</a> -->
+	
+	<!-- 상대경로 -->
+	<a href="./join">회원가입</a>
+	<a href="./login">로그인</a>
+<!-- 	<a href="join">회원가입하러가기</a> -->
+	
+</body>
+</html>
